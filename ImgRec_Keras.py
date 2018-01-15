ab457aaf03340c35e8d2bf64ea0dd7a094200521
xdzm1993 <531266541@qq.com>
2018-01-15 11:31:26 +0800

    Update ImgRec_Keras.py

--- ImgRec_Keras.py
+++ ImgRec_Keras.py
@@ -44,26 +44,36 @@ def model_create():
 
     num_classes = 10
 
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=input_image_shape))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(64, kernel_size=(3, 3),
-                     activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='sigmoid'))
-
-    model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
+    seed = 7  
+    np.random.seed(seed)  
+  
+    model = Sequential()  
+    model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=(224,224,3),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(MaxPooling2D(pool_size=(2,2)))  
+    model.add(Conv2D(128,(3,2),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(MaxPooling2D(pool_size=(2,2)))  
+    model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(MaxPooling2D(pool_size=(2,2)))  
+    model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(MaxPooling2D(pool_size=(2,2)))  
+    model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(512,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))  
+    model.add(MaxPooling2D(pool_size=(2,2)))  
+    model.add(Flatten())  
+    model.add(Dense(4096,activation='relu'))  
+    model.add(Dropout(0.5))  
+    model.add(Dense(4096,activation='relu'))  
+    model.add(Dropout(0.5))  
+    model.add(Dense(num_classes,activation='softmax'))  
+    model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])  
+    model.summary()  
 
     # model.add(Conv2D(32, kernel_size=(3, 3),
     #                  activation='relu',
