ba56146b8029ba635c5f97dfe8ff5eab29a11b06
xdzm1993 <531266541@qq.com>
2018-01-15 17:23:10 +0800

    Update ImgRec_Keras.py

--- ImgRec_Keras.py
+++ ImgRec_Keras.py
@@ -43,14 +43,16 @@ def model_create():
     # ## Define a Convolutional Neural Network
 
     num_classes = 10
-
+    dr=0.6
     seed = 7  
     np.random.seed(seed)  
     
     model = Sequential()  
-    model.add(Conv2D(32,(5,5),strides=(1,1),input_shape=input_image_shape,padding='valid',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(32,(5,5),strides=(1,1),input_shape=input_image_shape,padding='valid',activation='relu',kernel_initializer='uniform'))
+    model.add(Dropout(dr))
     model.add(MaxPooling2D(pool_size=(2,2)))  
-    model.add(Conv2D(64,(5,5),strides=(1,1),padding='valid',activation='relu',kernel_initializer='uniform'))  
+    model.add(Conv2D(64,(5,5),strides=(1,1),padding='valid',activation='relu',kernel_initializer='uniform'))
+    model.add(Dropout(dr))
     model.add(MaxPooling2D(pool_size=(2,2)))  
     model.add(Flatten())  
     model.add(Dense(100,activation='relu'))  
