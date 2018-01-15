8dfb83f9714e54e3a38ae8a3ab4a1b28a29e0061
xdzm1993 <531266541@qq.com>
2018-01-15 17:18:22 +0800

    Update ImgRec_Keras.py

--- ImgRec_Keras.py
+++ ImgRec_Keras.py
@@ -50,13 +50,13 @@ def model_create():
     model = Sequential()  
     model.add(Conv2D(32,(5,5),strides=(1,1),input_shape=input_image_shape,padding='valid',activation='relu',kernel_initializer='uniform'))  
     model.add(MaxPooling2D(pool_size=(2,2)))  
-	model.add(Conv2D(64,(5,5),strides=(1,1),padding='valid',activation='relu',kernel_initializer='uniform'))  
-	model.add(MaxPooling2D(pool_size=(2,2)))  
-	model.add(Flatten())  
-	model.add(Dense(100,activation='relu'))  
-	model.add(Dense(num_classes,activation='softmax'))  
-	model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])  
-	model.summary()    
+    model.add(Conv2D(64,(5,5),strides=(1,1),padding='valid',activation='relu',kernel_initializer='uniform'))  
+    model.add(MaxPooling2D(pool_size=(2,2)))  
+    model.add(Flatten())  
+    model.add(Dense(100,activation='relu'))  
+    model.add(Dense(num_classes,activation='softmax'))  
+    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])  
+    model.summary()    
     return model
 
     # You can view a summary of the network using the `summary()` function:
